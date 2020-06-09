9cfc205bd65d5e250ed70dd7f1e24c8e120ec492
SHUBHRAJIT DEY <shubhrajitdey98@gmail.com>
2020-06-09 23:31:35 +0530

    check

--- app/src/main/java/com/example/meo/MainActivity.java
+++ app/src/main/java/com/example/meo/MainActivity.java
@@ -2,22 +2,80 @@ package com.example.meo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class MainActivity extends AppCompatActivity {
-    MeowBottomNavigation meo;
+
+    private final static int ID_HOME = 1;
+    private final static int ID_EXPLORE = 2;
+    private final static int ID_MESSAGE = 3;
+    private final static int ID_NOTIFICATION = 4;
+    private final static int ID_ACCOUNT = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        meo=(MeowBottomNavigation)findViewById(R.id.bottom_nav);
-        meo.add(new MeowBottomNavigation.Model(1,R.drawable.homepage));
-        meo.add(new MeowBottomNavigation.Model(2,R.drawable.ic_shopping_cart_black));
-        meo.add(new MeowBottomNavigation.Model(3,R.drawable.orderhistory));
-        meo.add(new MeowBottomNavigation.Model(4,R.drawable.profile));
+        MeowBottomNavigation bottomNavigation = findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_EXPLORE, R.drawable.ic_explore));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_MESSAGE, R.drawable.ic_message));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_NOTIFICATION, R.drawable.ic_notification));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_ACCOUNT, R.drawable.ic_account));
+
+        bottomNavigation.setCount(ID_NOTIFICATION, "115");
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "clicked item : " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "showing item : " + item.getId(), Toast.LENGTH_SHORT).show();
+
+                String name;
+                switch (item.getId()) {
+                    case ID_HOME:
+                        name = "HOME";
+                        break;
+                    case ID_EXPLORE:
+                        name = "EXPLORE";
+                        break;
+                    case ID_MESSAGE:
+                        name = "MESSAGE";
+                        break;
+                    case ID_NOTIFICATION:
+                        name = "NOTIFICATION";
+                        break;
+                    case ID_ACCOUNT:
+                        name = "ACCOUNT";
+                        break;
+                    default:
+                        name = "";
+                }
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "reselected item : " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setCount(ID_NOTIFICATION, "115");
+
+        bottomNavigation.show(ID_NOTIFICATION, true);
     }
 }
